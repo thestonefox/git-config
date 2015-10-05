8074b3b8506a3c266c775bc5521c5127737d7d83
Harvey Ball <me@harveyball.com>
2015-10-05 11:57:46 +0100

    Fix issue with gitl1 not being created correctly

--- helpers.sh
+++ helpers.sh
@@ -1,4 +1,4 @@
-echo "gitl1() { git log --oneline "$@" ;}" >> ~/.bashrc
+echo "gitl1() { git log --oneline \$@; }" >> ~/.bashrc
 echo "alias gits='git status'" >> ~/.bashrc
 echo "alias gitb='git branch'" >> ~/.bashrc
 
